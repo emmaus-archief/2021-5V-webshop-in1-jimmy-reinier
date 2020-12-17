9ae4fe3f33b56bd96a6068ed71f7f9665666a2e7
Jimmychao49 <jimmychao49@gmail.com>
2020-12-17 13:00:23 +0000

    tables zouden nu moeten werken

--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -1,34 +1,26 @@
 DROP TABLE IF EXISTS products;
 DROP TABLE IF EXISTS geur;
 DROP TABLE IF EXISTS deksel;
+DROP TABLE IF EXISTS accessoire;
+DROP TABLE IF EXISTS regio;
+DROP TABLE IF EXISTS maten;
 CREATE TABLE products (
   id SERIAL PRIMARY KEY,
   code VARCHAR(15),
   name VARCHAR(255),
   description TEXT,
-  price NUMERIC(10, 2),
+  price NUMERIC(10,2),
   geur_id INTEGER,
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
->>>>>>> cd1f49f9c23ee67d71ecb394c8236f6732f65d07
   deksel_id INTEGER,
   accessoire_id INTEGER,
   regio_id INTEGER,
   maten_id INTEGER
-<<<<<<< HEAD
->>>>>>> 52a5bd3daae2824bf34c31e00e830d2e0ad7534a
-=======
-=======
-  deksel_id INTEGER
->>>>>>> 6e0d3537fdce546a2e888a8dba8c2a0546a93e97
->>>>>>> cd1f49f9c23ee67d71ecb394c8236f6732f65d07
 );
 
 CREATE TABLE geur (
   id SERIAL PRIMARY KEY,
-    beschrijving TEXT,
-  geur_naam TEXT
+  geur_naam TEXT,
+  beschrijving TEXT
 );
 
 CREATE TABLE deksel (
--- sql/seed.sql
+++ sql/seed.sql
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 -- generared using
 -- curl "https://api.mockaroo.com/api/910b6c20?count=100&key=90eac760" > seed.sql
 --
@@ -7,58 +7,19 @@
 
 --products
 
-<<<<<<< HEAD
-INSERT INTO products (code, name, description, price, geur_id, deksel_id, accesoire_id, regio_id, maten_id)
-VALUES ('#01010101EUS', 'The Bottle', 'De originele Bottle van onze collectie, met een tijdloze en onopvallend design, past het perfect in elke ruimte.',49.99,01,01,01,01);
-INSERT INTO products (code, name, description, price, geur_id, deksel_id, accesoire_id, regio_id, maten_id)
-VALUES ('#02010101EUS', 'Magic Ball', 'Deze elegante bal geeft uw kamer onmiddelijk een fijne geur naar uw keuze.',119.99,01,01,01,01,01);
-INSERT INTO products (code, name, description, price, geur_id, deksel_id, accesoire_id, regio_id, maten_id)
-VALUES ('#03010101EUS', 'Pentagonal Bliss', 'Vijfhoeken zijn in. Geef je kamer een frisse geur.',89.99,01,01,01,01,01);
-INSERT INTO products (code, name, description, price, geur_id, deksel_id, accesoire_id, regio_id, maten_id)
-=======
-INSERT INTO products (code, name, description, price, geur_id, deksel_id, accessoire_id, regio_id, maten_id)
-VALUES ('#01010101EUS', 'The Bottle', 'De originele Bottle van onze collectie, met een tijdloze en onopvallend design, past het perfect in elke ruimte.',49.99,01,01,01,01,01);
-INSERT INTO products (code, name, description, price, geur_id, deksel_id, accessoire_id, regio_id, maten_id)
-VALUES ('#02010101EUS', 'Magic Ball', 'Deze elegante bal geeft uw kamer onmiddelijk een fijne geur naar uw keuze.',119,01,01,01,01,01);
-INSERT INTO products (code, name, description, price, geur_id, deksel_id, accessoire_id, regio_id, maten_id)
-VALUES ('#03010101EUS', 'Pentagonal Bliss', 'Vijfhoeken zijn in. Geef je kamer een frisse geur.',89,01,01,01,01,01);
-INSERT INTO products (code, name, description, price, geur_id, deksel_id, accessoire_id, regio_id, maten_id)
->>>>>>> cd1f49f9c23ee67d71ecb394c8236f6732f65d07
-VALUES ('#04010101EUS', 'Cilinder', 'Het is een cilinder, met een geur.',59.99,01,01,01,01,01);
-insert into products (code, name, description, price, geur_id, deksel_id, accessoire_id, regio_id, maten_id) 
-values ('#05010101EUS','Cube','Een simpele kubus. Scherpe hoeken voor een scherpe geur.',69.99,01,01,01,01,01);
-<<<<<<< HEAD
-insert into products (code, name, description, price, geur_id, deksel_id, accessoire_id, regio_id, maten_id) 
-values ('13925-100', 'etium nisl ut volutpat sapien arcu sed augue aliquam erat volutpat in', 455, 2, 3);
-insert into products (code, name, description, price, geur_id, deksel_id, accessoire_id, regio_id, maten_id) 
-=======
-insert into products (code, name, description, price, geur_id, deksel_id, accesoire_id, regio_id, maten_id) 
-values ('#06010101NAS','Pyramid','',99.99,01,01,01,01,01);
-insert into products (code, name, description, price, geur_id, deksel_id, accesoire_id, regio_id, maten_id) 
-<<<<<<< HEAD
-values ('#07010101NAS', 'Lovey Heart', '', 312, 3, 2);
-insert into products (code, name, description, price, geur_id, deksel_id, accesoire_id, regio_id, maten_id) 
-values ('#08010101NAS', 'Oval Elegance', 'ligula pellentesque ultrices phasellus id sapien in sapien iaculis congue vivamus', 135, 3, 1);
-insert into products (code, name, description, price, geur_id, deksel_id, accesoire_id, regio_id, maten_id) 
-=======
->>>>>>> 6e0d3537fdce546a2e888a8dba8c2a0546a93e97
-values ('0781-7066', 'Balearica pavonina', 'turpis integer aliquet massa id lobortis convallis', 312, 3, 2);
-insert into products (code, name, description, price, geur_id, deksel_id, accessoire_id, regio_id, maten_id) 
-values ('51393-7223', 'Lepus arcticus', 'ligula pellentesque ultrices phasellus id sapien in sapien iaculis congue vivamus', 135, 3, 1);
-insert into products (code, name, description, price, geur_id, deksel_id, accessoire_id, regio_id, maten_id) 
->>>>>>> cd1f49f9c23ee67d71ecb394c8236f6732f65d07
-values ('68084-635', 'orttitor id consequat in consequat ut nulla sed accumsan felis', 270, 3, 2);
-insert into products (code, name, description, price, geur_id, deksel_id, accessoire_id, regio_id, maten_id) 
-values ('49944-001', ' ullamcorper purus sit amet', 179, 3, 1);
-insert into products (code, name, description, price, geur_id, deksel_id, accessoire_id, regio_id, maten_id) 
-values ('66129-430', 'Ictalurus furcatus', 'a feugiat et eros vestibulum ac est lacinia nisi venenati', 59, 1, 3);
-insert into products (code, name, description, price, geur_id, deksel_id, accessoire_id, regio_id, maten_id) 
-values ('52536-300', 'Dipodomys deserti', 'auctor sed tristique in tempus sit amet', 131, 2, 3);
-insert into products (code, name, description, price, geur_id, deksel_id, accessoire_id, regio_id, maten_id) 
-values ('55289-060', 'Bucephala clangula', 'morbi ut odio cras mi pede malesuada in imperdie', 424, 1, 3);
-insert into products (code, name, description, price, geur_id, deksel_id, accessoire_id, regio_id, maten_id) 
-values ('16590-147', 'a suspendisse ornare consequat lectus in est risus auctor sed', 402, 1, 1);
 
+INSERT INTO products (code, name, description, price, geur_id, deksel_id, accessoire_id, regio_id, maten_id)
+VALUES ('#010101EUS', 'The Bottle', 'De originele Bottle van onze collectie, met een tijdloze en onopvallend design, past het perfect in elke ruimte.', 49.99, 1, 1, 1, 1, 1);
+INSERT INTO products (code, name, description, price, geur_id, deksel_id, accessoire_id, regio_id, maten_id)
+VALUES ('#010101EUS', 'Magic Ball', 'Deze elegante bal geeft uw kamer onmiddelijk een fijne geur naar uw keuze.', 59.99, 1, 1, 1, 1, 1);
+INSERT INTO products (code, name, description, price, geur_id, deksel_id, accessoire_id, regio_id, maten_id)
+VALUES ('#030101EUS', 'Pentagonal Bliss', 'Vijfhoeken zijn in. Geef je kamer een frisse geur.', 69.99, 3, 1, 1, 1, 1);
+INSERT INTO products (code, name, description, price, geur_id, deksel_id, accessoire_id, regio_id, maten_id)
+VALUES ('#010101EUS', 'Cilinder', 'Het is een cilinder, met een geur.', 79.99, 3, 1, 1, 1, 1);
+insert into products (code, name, description, price, geur_id, deksel_id, accessoire_id, regio_id, maten_id) 
+values ('#010101EUS','Cube','Een simpele kubus. Scherpe hoeken voor een scherpe geur.', 89.99, 3, 1, 1, 1, 1);
+insert into products (code, name, description, price, geur_id, deksel_id, accessoire_id, regio_id, maten_id) 
+values ('#030101NAS','Pyramid','het is een pyramide.', 49.99, 3, 1, 1, 2, 1);
 
 --CONTENTS VAN GEUREN
 
@@ -78,17 +39,12 @@ INSERT INTO deksel (deksel_kleur) VALUES ('Forest Green');
 INSERT INTO deksel (deksel_kleur) VALUES ('Honeycomb Yellow');
 INSERT INTO deksel (deksel_kleur) VALUES ('Heavenly White');
 
---accessoires
+--accessoire
 
-<<<<<<< HEAD
-INSERT INTO accessoires (accessoire_naam) VALUES ('Verlichte versie');
-INSERT INTO accessoires (accessoire_naam) VALUES ('Kerstmuts');
-INSERT INTO accessoires (accessoire_naam) VALUES ('Zomer Hoed')
-=======
+INSERT INTO accessoire (accessoire_naam) VALUES ('geen');
 INSERT INTO accessoire (accessoire_naam) VALUES ('Verlichte versie');
 INSERT INTO accessoire (accessoire_naam) VALUES ('Kerstmuts');
-INSERT INTO accessoire (accessoire_naam) VALUES ('');
->>>>>>> cd1f49f9c23ee67d71ecb394c8236f6732f65d07
+INSERT INTO accessoire (accessoire_naam) VALUES ('Zomer Hoed');
 
 --regio
 
@@ -107,11 +63,22 @@ INSERT INTO maten (maten_grootte) VALUES ('M');
 INSERT INTO maten (maten_grootte) VALUES ('L');
 INSERT INTO maten (maten_grootte) VALUES ('XL');
 
-
-SELECT products.id, products.code, products.name, products.description, products.price, geur.geur_naam, geur.beschrijving, deksel.deksel_kleur FROM products 
-LEFT OUTER JOIN geur ON geur.id = products.geur_id
+--volledige informatie van een artikel
+SELECT products.id, products.code, products.name, products.description, price, geur.geur_naam, accessoire.accessoire_naam, regio.regio_naam, maten.maten_grootte FROM products 
+LEFT OUTER JOIN geur ON geur.id = products.geur_id 
 LEFT OUTER JOIN deksel ON deksel.id = products.deksel_id
-LEFT OUTER JOIN accessoire ON accessoire.id = products.accessoire_id
+LEFT OUTER JOIN accessoire ON accessoire.id = products.accessoire_id 
 LEFT OUTER JOIN regio ON regio.id = products.regio_id
 LEFT OUTER JOIN maten ON maten.id = products.maten_id;
+
+--meer producten uit deze regio
+SELECT products.name AS Meer_producten_uit_deze_regio, products.price FROM products 
+LEFT OUTER JOIN regio ON regio.id = products.regio_id 
+WHERE regio.id = 1;
+
+--meer producten met deze geur
+SELECT products.name AS meer_producten_met_deze_geur, products.price FROM products 
+LEFT OUTER JOIN geur ON geur.id = products.geur_id 
+WHERE geur.id = 1;
+
 
